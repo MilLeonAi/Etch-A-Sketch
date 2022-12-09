ec4c5343d49809d8f90f714e6161a584192a710a
MilLeonAi <noellim9@gmail.com>
2022-12-09 08:06:04 +0700

    added html and css

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let board = document.querySelector(".board");
+createBoard(64);
 
+// board created for the pixel
 function createBoard(num){
     // Clear the board if there is an element
+    let board = document.querySelector(".board");
     while (board.firstElementChild){
         board.firstElementChild.remove();
     }
-    
-    for (let i = 0; i < num * num; i ++)
+    // Loop through to create the divs
+    for (let i = 0; i < num * num; i++)
     {
-        let grid = document.createElement("div");
-        grid.classLName = "foo";
-        board.appendChild(grid);
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        let box = document.createElement("div");
+        box.style.height = `calc(500px / ${num})`;
+        box.style.width = `calc(500px / ${num})`;
+        box.style.backgroundColor = `#${randomColor}`;
+        board.appendChild(box);
     }
+
 
 }
